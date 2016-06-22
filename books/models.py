233f3b52f670b547693f9f51c2998e48d9830968
QQYES <qqyesqq1@126.com>
2016-06-22 21:44:11 +0800

    comment

--- books/models.py
+++ books/models.py
@@ -4,6 +4,12 @@ from django.db import models
 
 
 # Create your models here.
+
+class BookManager(models.Manager):
+    def title_count(self, keyword):
+        return self.filter(title__icontains=keyword).count()
+
+
 class Publisher(models.Model):
     name = models.CharField(max_length=30)
     address = models.CharField(max_length=50)
@@ -28,11 +34,60 @@ class Author(models.Model):
         return u'%s %s' % (self.first_name, self.last_name)
 
 
+# First, define the Manager subclass.
+class DahlBookManager(models.Manager):
+    def get_query_set(self):
+        return super(DahlBookManager, self).get_query_set().filter(author='Roald Dahl')
+
+
+# Then hook it into the Book model explicitly.
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
     authors = models.ManyToManyField(Author)
     publisher = models.ForeignKey(Publisher)
     publication_date = models.DateField()
+    num_pages = models.IntegerField(blank=True, null=True)
+    # objects = BookManager()
+    objects = models.Manager()  # The default manager.**
+    dahl_objects = DahlBookManager()  # The Dahl-specific manager.**
 
     def __unicode__(self):
         return self.title
+
+
+class MaleManager(models.Manager):
+    def get_query_set(self):
+        return super(MaleManager, self).get_query_set().filter(sex='M')
+
+
+class FemaleManager(models.Manager):
+    def get_query_set(self):
+        return super(FemaleManager, self).get_query_set().filter(sex='F')
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    birth_date = models.DateField()
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    state = USStateField() # Yes, this is U.S.-centric...
+
+    def baby_boomer_status(self):
+        "Returns the person's baby-boomer status."
+        import datetime
+        if datetime.date(1945, 8, 1) <= self.birth_date <= datetime.date(1964, 12, 31):
+            return "Baby boomer"
+        if self.birth_date < datetime.date(1945, 8, 1):
+            return "Pre-boomer"
+        return "Post-boomer"
+
+    def is_midwestern(self):
+        "Returns True if this person is from the Midwest."
+        return self.state in ('IL', 'WI', 'MI', 'IN', 'OH', 'IA', 'MO')
+
+    def _get_full_name(self):
+        "Returns the person's full name."
+        return u'%s %s' % (self.first_name, self.last_name)
+    full_name = property(_get_full_name)
