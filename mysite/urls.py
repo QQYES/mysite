c702ba0955501b54f5372cd0680b9555c9f320e9
QQYES <qqyesqq1@126.com>
2016-06-23 16:37:47 +0800

    comm

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.conf.urls import url, patterns
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 # from mysite.views import hello, current_datetime, hours_ahead, display_meta
 # from books.views import search
@@ -36,6 +37,8 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
+
+urlpatterns += patterns('', (r'^about/$', TemplateView.as_view(template_name="about.html")), )
 
 urlpatterns += patterns('mysite.views',
                         url(r'^hello/$', 'hello'),
